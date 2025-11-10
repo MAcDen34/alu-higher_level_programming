2e4f456f09c572d912741163000507b05aa8f707
Denzel_34 <d.ngabo@alustudent.com>
2025-11-10 14:09:38 +0200

    Fix: Improve docstring formatting in __str__ method of Rectangle class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -65,7 +65,9 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """ Return the string representation of the rectangle using '#' characters. """
+        """ 
+        Returning string representation of the rectangle using '#' characters.
+      """
         if self.__width == 0 or self.__height == 0:
             return ""
 
