2f2908426010595679b1c268c4dadb4498f5a574
Denzel_34 <d.ngabo@alustudent.com>
2025-11-10 13:58:49 +0200

    Fix: Ensure consistent formatting in Rectangle class methods

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -39,9 +39,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
+    
     def perimeter(self):
         """Calculate and return the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
