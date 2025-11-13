41decb05911875af08e6133deba2549465830d9b
Denzel_34 <d.ngabo@alustudent.com>
2025-11-13 22:42:05 +0200

    Fix: Optimize string construction in __str__ method of Rectangle class

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -69,10 +69,9 @@ initialize the Rectangle class with width and height attributes
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        
         rectangle_str = []
         for _ in range(self.__height):
-            rectangle_str += "#" * self.__width + "\n"
+            rectangle_str.append("#" * self.__width)
         return "\n".join(rectangle_str)
 
     def __repr__(self):
