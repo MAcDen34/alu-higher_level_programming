9d3328b50b212d878939d37309ec3abf5e75e9b8
MAcDen34 <d.ngabo@alustudent.com>
2026-02-16 18:06:59 +0200

    feat: add JS scripts for recursion, object manipulation, and module exports

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function secondBiggest (args) {
+  if (args.length === 0 || args.length === 1) {
+    return 0;
+  }
+  const sortedArray = args.sort((a, b) => b - a);
+
+  return sortedArray[1];
+}
+
+console.log(secondBiggest(args));
